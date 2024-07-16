86d5f90fb8f967f4d6724e15b1eb27b2e834bfab
pedromedici-uvv <146857181+pedromedici-uvv@users.noreply.github.com>
2024-07-16 12:44:52 -0300

    Add project files.

---
+++ Auctions/Entities/Auction.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuctionService.Entities;
+[Table("Auctions")]
+public class Auction
+{
+    public Guid Id { get; set; }
+    public int ReservePrice { get; set; } = 0;
+    public string Seller { get; set; }
+    public string Winner { get; set; }
+    public int? SoldAmount { get; set; }
+    public int? CurrentHighBid { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    public DateTime AuctionEnd { get; set; }
+    public Item Item { get; set; }
+    public Status Status { get; set; }
+}
