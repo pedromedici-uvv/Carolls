f9661c3920642b74337c9b4e068cd226cfc738a6
pedromedici-uvv <146857181+pedromedici-uvv@users.noreply.github.com>
2024-07-16 17:57:39 -0300

    adding search params

---
+++ SearchService/Controllers/SearchController.cs
@@ -0,0 +1,58 @@
+﻿using DnsClient;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Helpers;
+using SearchService.Models;
+
+namespace SearchService.Controllers
+{
+    [Route("api/search")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        [HttpGet] public async Task<ActionResult<List<Item>>> SearchItems([FromQuery]SearchParams searchParams)
+        {
+            var query = DB.PagedSearch<Item, Item>();
+
+
+            if (!string.IsNullOrEmpty(searchParams.SearchTerm))
+            {
+                query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
+            }
+
+            query = searchParams.OrderBy switch
+            {
+                "make" => query.Sort(x => x.Ascending(a => a.Make)),
+                "new" => query.Sort(x => x.Ascending(a => a.CreatedAt)),
+                _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+            };
+
+            query = searchParams.FilterBy switch
+            {
+                "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+                "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd < DateTime.UtcNow),
+                _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+            };
+
+            if (!string.IsNullOrEmpty(searchParams.Seller))
+            {
+                query.Match(x => x.Seller == searchParams.Seller);
+            }
+            if (!string.IsNullOrEmpty(searchParams.Winner))
+            {
+                query.Match(x => x.Winner == searchParams.Winner);
+            }
+
+            query.PageNumber(searchParams.PageNumber).PageSize(searchParams.PageSize);
+            var results= await query.ExecuteAsync();
+
+            return Ok(new
+            {
+                results = results.Results,
+                pageCount = results.PageCount,
+                totalCount = results.TotalCount
+            });
+        }
+    }
+}
